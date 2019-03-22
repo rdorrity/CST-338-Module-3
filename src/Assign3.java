358274fc8ca376ef1a02f9e9b99c34777e3c6985
Ryan <rdorrity@gmail.com>
2019-03-21 22:14:51 -0700

    Update Assign3.java
    I mutated the constructor

--- src/Assign3.java
+++ src/Assign3.java
@@ -1,124 +1,144 @@
 
 /**
- * 
+ *
  * @author Ryan Dorrity, Cody Young, Sara Kazemi, Nathan Warren-Acord
  * @version 03/20/2019
  */
-public class Assign3 
+public class Assign3
 {
-    
     public static void main(String[] args)
     {
         Card testA = new Card();
+        //System.out.println(testA.toString());
         Card testB = new Card('2', Card.Suit.clubs);
+        //System.out.println(testB.toString());
         Card testC = new Card('0', Card.Suit.hearts);
-        
-        System.out.println(testA.toString());
-        System.out.println(testB.toString());
-        System.out.println(testC.toString());
+        //System.out.println(testC.toString());
     }
-    
 }
 /**
- * 
+ *
  * @author Ryan Dorrity, Cody Young, Sara Kazemi, Nathan Warren-Acord
  * @version 03/20/2019
  */
-class Card 
-{    
+class Card
+{
+    char[] cardRank = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J',
+            'Q', 'K'};
     enum Suit {clubs, diamonds, hearts, spades};
     Suit suit;
     char value;
-    boolean errorFlag;
-    
+    boolean errorFlag = true;
+
     /**
-     * 
+     *
      */
     public Card()
     {
         value = 'A';
         suit = Suit.spades;
+        errorFlag = isValid('A', Suit.spades);
+        System.out.println(toString());
     }
-    
+
     /**
-     * 
+     *
      * @param value
      * @param suit
      */
     public Card(char value, Suit suit)
     {
-        this.value = value;
-        this.suit = suit;
+        errorFlag = set(value, suit);
+        System.out.println(toString());
     }
-    
+
     /**
-     * 
+     *
      */
     public String toString()
     {
-        return Character.toString(value) + " of " + suit;
+        if (errorFlag == true)
+        {
+            return "Invalid";
+        }
+        else
+            return Character.toString(value) + " of " + suit;
     }
-    
+
     /**
-     * 
+     *
      * @param value
      * @param suit
      * @return
      */
     boolean set(char value, Suit suit)
     {
-    	return false;
+        errorFlag = isValid(value, suit);
+        if (errorFlag == false)
+        {
+            this.value = value;
+            this.suit = suit;
+            return errorFlag;
+        }
+        else
+            this.suit = suit;
+        return errorFlag;
     }
-    
+
     /**
-     * 
+     *
      * @return
      */
     public Suit getSuit()
     {
-    	return suit;
+        return suit;
     }
-    
+
     /**
-     * 
+     *
      * @return
      */
     public char getValue()
     {
-    	return value;
+        return value;
     }
-    
+
     /**
-     * 
+     *
      * @return
      */
     public boolean getFlag()
     {
-    	return errorFlag;
+        return errorFlag;
     }
-    
+
     /**
-     * 
+     *
      * @param card
      * @return
      */
     public boolean equals(Card card)
     {
-    	return false;    	
+        return false;
     }
-    
+
     /**
-     * 
+     *
      * @param value
      * @param suit
      * @return
      */
-    private boolean isValid(char value, Suit suit)    
+    private boolean isValid(char value, Suit suit)
     {
-    	if (suit.equals(this.suit))
-    	{
-    		System.out.println("True");
-    	}
-    	return false;
+        for (int i = 0; i < cardRank.length; i++)
+        {
+            if (cardRank[i] == value)
+            {
+                //System.out.println("Valid");
+                return false;
+            }
+        }
+        //System.out.println("Invalid");
+        return true;
     }
 }
